d408a3d62a637bcec6cbc5b5cde6d7642e244f91
Frederic Esser <frederic.esser@googlemail.com>
2025-05-28 10:40:20 +0200

    Added routes
    Bundle and Creator Components added and linked the buttons to those routes.
    The dark-mode service was also moved to the shared/services directory.

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import {FormsModule} from '@angular/forms';
 import {TuiAppearance, TuiButton, TuiHint, TuiRoot} from '@taiga-ui/core';
 import {TuiFade} from '@taiga-ui/kit';
 import {TuiNavigation} from '@taiga-ui/layout';
-import {DarkModeService} from './shared/dark-mode.service';
+import {DarkModeService} from './shared/services/dark-mode.service';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, TuiButton, TuiNavigation, TuiFade, TuiRoot, TuiHint, TuiAppearance],
+  imports: [FormsModule, TuiButton, TuiNavigation, TuiFade, TuiRoot, TuiHint, TuiAppearance, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {CreatorComponent} from './routes/creator/creator.component';
+import {BundleComponent} from './routes/bundle/bundle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'creator', component: CreatorComponent },
+  { path: 'bundle', component: BundleComponent },
+];
